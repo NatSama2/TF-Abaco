97a58ecaa5ff92f58c033fead60ae997e6759e4d
Tamarindo <t.aviles.bosoni@gmail.com>
2023-10-25 09:30:00 -0300

    taiwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ export default {
   ],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      'grisMuyClaro': '#E6E6E6',
+      'gris-oscuro':'#4d4d4d',
+      'verde-oscuro': '#065624',
       'naranja': '#f58634',
       'verde-claro': '#8cc63f',
       'amarillo': '#d2de38',
+      'negro': '#272323'
+
     },
     extend: {},
   },
